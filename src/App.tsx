936498dff04afd3885201fdb107efd7c2add8a37
sondresjolyst <sosj@equinor.com>
2022-05-02 08:58:42 +0200

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import logo from './logo.svg'
 import './App.css';
+
+function Home() {
+  return (
+    <h1>test</h1>
+  )
+}
+
+function Foo() {
+  return (
+    <h2>foo</h2>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='app'>
+      <header className='App-header'>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -15,10 +28,27 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        >       Learn React     </a>
       </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/foo">foo</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/foo" element={<Foo />}></Route>
+          </Routes>
+
+        </div>
+      </Router>
     </div>
   );
 }
