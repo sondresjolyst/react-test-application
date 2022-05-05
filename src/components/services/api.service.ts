32339aaac11c20addc5a651c9c32404116e8c760
sondresjolyst <sosj@equinor.com>
2022-05-05 08:35:20 +0200

    eslint

--- src/components/services/api.service.ts
+++ src/components/services/api.service.ts
@@ -1,21 +1,26 @@
 import axios from 'axios';
 
+/**
+ * @return {json} API service that returns JSON
+ */
 export class Api {
+  client: any;
+  apiUrl: string;
 
-    client: any;
-    apiUrl: string;
+  /**
+   * constructor
+   */
+  constructor() {
+    this.client = null;
+    this.apiUrl = 'http://express-backend-typescript-example.com/api/';
+  }
 
-    constructor() {
-        this.client = null;
-        this.apiUrl = 'http://express-backend-typescript-example.com/api/';
-    }
+  init = () => {
+    this.client = axios.create({baseURL: this.apiUrl, timeout: 31000});
+    return this.client;
+  };
 
-    init = () => {
-        this.client = axios.create({baseURL: this.apiUrl, timeout: 31000});
-        return this.client;
-    };
-
-    getFactOfTheDay = () => {
-        return this.init().get('/example/catFact');
-    }
+  getFactOfTheDay = () => {
+    return this.init().get('/example/catFact');
+  };
 }
