5b0b8d83986feb93a4ce6a2d098e18ef0bbcb744
sondresjolyst <sosj@equinor.com>
2022-05-09 14:56:55 +0200

    added marking between components

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -14,13 +14,13 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <div className='uk-container uk-container-xlarge'>
+        <div className='uk-container container-large'>
           <Fact />
         </div>
-        <div className='uk-container uk-container-xlarge'>
+        <div className='uk-container container-large'>
           <Facts />
         </div>
-        <div className='uk-container uk-container-xlarge'>
+        <div className='uk-container container-large'>
           <Breeds />
         </div>
       </div>
